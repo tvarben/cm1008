408b32449e0c0b355f425a04694d9582eb92b503
David Vilselius <tvaarben@gmail.com>
2025-03-31 14:02:46 +0200

    git pull test

--- helloWorld.c
+++ helloWorld.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-int main()
-{
-    printf("Hello World /Adam\n");
-    printf("Hej hej/Erik \n");
-    printf("Pull request test\n");
-    printf("test again :)\n");
-    pritnf("Ny Commit \n");
-    return 0;
+int main() {
+  printf("Hello World /Adam\n");
+  printf("Hej hej/Erik \n");
+  printf("Pull request test\n");
+  printf("test again :)\n");
+  printf("Ny Commit \n");
+  printf("David test pull\n");
+  return 0;
 }
