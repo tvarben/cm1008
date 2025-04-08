6c911aac229d6966d2ac965c907a418d07ad1c70
David <tvaarben@gmail.com>
2025-04-08 11:59:34 +0200

    changed name of close function

--- src/main.c
+++ src/main.c
@@ -1,51 +1,53 @@
-#include <stdio.h>
 #include <SDL2/SDL.h>
 #include <stdbool.h>
+#include <stdio.h>
 
 #define WINDOW_WIDTH 1160
 #define WINDOW_HEIGHT 700
 
 typedef struct {
-	SDL_Window *pWindow;
-	SDL_Renderer *pRenderer;
-}Game;
+  SDL_Window *pWindow;
+  SDL_Renderer *pRenderer;
+} Game;
 
 int initiate(Game *pGame) {
-	
-	SDL_Init(SDL_INIT_EVERYTHING);
 
-	pGame->pWindow = SDL_CreateWindow("Hello World", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
-	pGame->pRenderer = SDL_CreateRenderer(pGame->pWindow, -1, 0);
+  SDL_Init(SDL_INIT_EVERYTHING);
+
+  pGame->pWindow =
+      SDL_CreateWindow("Hello World", SDL_WINDOWPOS_CENTERED,
+                       SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
+  pGame->pRenderer = SDL_CreateRenderer(pGame->pWindow, -1, 0);
 }
 
-void close(Game *pGame) {
-	SDL_DestroyWindow(pGame->pWindow);
-	SDL_DestroyRenderer(pGame->pRenderer);
-	SDL_Quit();
+void closeGame(Game *pGame) {
+  SDL_DestroyWindow(pGame->pWindow);
+  SDL_DestroyRenderer(pGame->pRenderer);
+  SDL_Quit();
 }
 
 void run(Game *pGame) {
-	bool isRunning = true;
-	SDL_Event event;
+  bool isRunning = true;
+  SDL_Event event;
 
-	while(isRunning) {
-		while(SDL_PollEvent(&event)){
-			switch(event.type) {
-				case SDL_QUIT:
-					isRunning = false;
-					break;
-			}
-		}
-	}
+  while (isRunning) {
+    while (SDL_PollEvent(&event)) {
+      switch (event.type) {
+      case SDL_QUIT:
+        isRunning = false;
+        break;
+      }
+    }
+  }
 }
 
-int main(int argv, char** args) {
-	Game game = {NULL, NULL};
-	initiate(&game);
-	
-	bool isRunning = true;
-	run(&game);
+int main(int argv, char **args) {
+  Game game = {NULL, NULL};
+  initiate(&game);
 
-	close(&game);
-	return 0;
+  bool isRunning = true;
+  run(&game);
+
+  closeGame(&game);
+  return 0;
 }
