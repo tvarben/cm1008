0c2b51de5c3983260166b7600b0e13315d7848fe
alexanderbruze <alexanderbruze@gmail.com>
2025-05-09 17:53:14 +0200

    Changend indentation in bullet.c

--- lib/src/bullet.c
+++ lib/src/bullet.c
@@ -11,81 +11,75 @@ Bullet projectiles[MAX_PROJECTILES]; // 100 bullets can be active at the time
 #define projectile_length 2
 
 void spawn_projectile(float x, float y, float dx, float dy) {
-  for (int i = 0; i < MAX_PROJECTILES; i++) {
-    if (!projectiles[i].active) {
-      projectiles[i].x = x;
-      projectiles[i].y = y;
-      projectiles[i].vx = dx;
-      projectiles[i].vy = dy;
-      projectiles[i].active = 1;
-      projectiles[i].rect.w = projectile_width; // size of projectile
-      projectiles[i].rect.h = projectile_length;
-      //printf("spawning projectile\n");
-      return;
+    for (int i = 0; i < MAX_PROJECTILES; i++) {
+        if (!projectiles[i].active) {
+            projectiles[i].x = x;
+            projectiles[i].y = y;
+            projectiles[i].vx = dx;
+            projectiles[i].vy = dy;
+            projectiles[i].active = 1;
+            projectiles[i].rect.w = projectile_width; // size of projectile
+            projectiles[i].rect.h = projectile_length;
+            //printf("spawning projectile\n");
+            return;
+        }
     }
-  }
 }
 
 void update_projectiles(Uint32 delta_time) {
-  for (int i = 0; i < MAX_PROJECTILES; i++) {
-    if (projectiles[i].active) {
-      projectiles[i].x += projectiles[i].vx;// * delta_time;
-      projectiles[i].y += projectiles[i].vy;// * delta_time;
-      //printf("Updating projectiles\n");
+    for (int i = 0; i < MAX_PROJECTILES; i++) {
+        if (projectiles[i].active) {
+            projectiles[i].x += projectiles[i].vx;// * delta_time;
+            projectiles[i].y += projectiles[i].vy;// * delta_time;
+            //printf("Updating projectiles\n");
 
-      // Deactivate if off screen (based on wndiow size)
-      if (projectiles[i].x < 0 || projectiles[i].x > WINDOW_WIDTH ||
-          projectiles[i].y < 0 || projectiles[i].y > WINDOW_HEIGHT) {
-        projectiles[i].active = 0;
-        //printf("deactivating projectile\n");
-      }
+            // Deactivate if off screen (based on wndiow size)
+            if (projectiles[i].x < 0 || projectiles[i].x > WINDOW_WIDTH ||
+                projectiles[i].y < 0 || projectiles[i].y > WINDOW_HEIGHT) {
+                projectiles[i].active = 0;
+                //printf("deactivating projectile\n");
+            }
 
-      // Update rect position for rendering
-      projectiles[i].rect.x = (int)projectiles[i].x;
-      projectiles[i].rect.y = (int)projectiles[i].y;
-    } // casting int gets rid of compiler warnings
-  }
+            // Update rect position for rendering
+            projectiles[i].rect.x = (int)projectiles[i].x;
+            projectiles[i].rect.y = (int)projectiles[i].y;
+        } // casting int gets rid of compiler warnings
+    }
 }
 
 void render_projectiles(SDL_Renderer *renderer) {
-  SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255); // Yellow
+    SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255); // Yellow
 
-  for (int i = 0; i < MAX_PROJECTILES; i++) {
-    if (projectiles[i].active) {
-      SDL_RenderFillRect(renderer, &projectiles[i].rect);
-      //printf("rendering projectile\n");
+    for (int i = 0; i < MAX_PROJECTILES; i++) {
+        if (projectiles[i].active) {
+            SDL_RenderFillRect(renderer, &projectiles[i].rect);
+            //printf("rendering projectile\n");
+        }
     }
-  }
 }
 
 SDL_Rect getRectBullet(Bullet *pBullet) { return pBullet->rect; }
 
 void getProjectileRects(SDL_Rect rectArray[]) {
-  SDL_Rect emptyRect ={0,0,0,0};
-  for (int i = 0; i < MAX_PROJECTILES; i++) {
-    if (!projectiles[i].active)
-    {
-      rectArray[i] = emptyRect;
+    SDL_Rect emptyRect ={0,0,0,0};
+    for (int i = 0; i < MAX_PROJECTILES; i++) {
+        if (!projectiles[i].active) {
+            rectArray[i] = emptyRect;
+        }
+        else{
+            rectArray[i] = projectiles[i].rect;
     }
-    else
-    {
-      rectArray[i] = projectiles[i].rect;
+}
+
+    return;
+}
+void resetAllBullets() {
+    for(int i=0;i<MAX_PROJECTILES;i++) {
+        projectiles[i].active=false;
     }
-
-  }
-
-  return;
+    return;
 }
-void resetAllBullets()
-{
-  for(int i=0;i<MAX_PROJECTILES;i++)
-  {
-    projectiles[i].active=false;
-  }
-  return;
-}
-void removeProjectile(int i)
-{
-  projectiles[i].active = false;
-  return;
+void removeProjectile(int i) {
+    projectiles[i].active = false;
+    return;
 }
