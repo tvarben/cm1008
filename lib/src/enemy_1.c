cfe3746f0669fb263594bf6cd2f828afb59de6ea
BitsAndBytes <adam.aldarwich@gmail.com>
2025-05-15 14:05:37 +0200

    Fixed indentaion style. Followed a standard style across all project file

--- lib/src/enemy_1.c
+++ lib/src/enemy_1.c
@@ -1,24 +1,24 @@
 #include "enemy_1.h"
 #include "ship_data.h"
 #include <SDL2/SDL_image.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 #define ENEMY_1_PATH "../lib/resources/ufo.png"
 #define INITIALSPEED 3;
-struct enemyImage{
+struct enemyImage {
     SDL_Renderer *pRenderer;
-    SDL_Texture *pTexture;    
+    SDL_Texture *pTexture;
 };
 
-struct enemy{
+struct enemy {
     float x, y, vx, vy;
     int health;
     int damage;
     bool active;
-    int window_width,window_height;
+    int window_width, window_height;
     SDL_Renderer *pRenderer;
     SDL_Texture *pTexture;
     SDL_Rect rect;
@@ -27,17 +27,14 @@ struct enemy{
 
 static void getStartValues(Enemy *pEnemy);
 
-EnemyImage *initiateEnemy(SDL_Renderer *pRenderer)  
-{
-    static EnemyImage* pEnemyImage = NULL;
-    if(pEnemyImage==NULL)
-    {
+EnemyImage *initiateEnemy(SDL_Renderer *pRenderer) {
+    static EnemyImage *pEnemyImage = NULL;
+    if (pEnemyImage == NULL) {
         pEnemyImage = malloc(sizeof(struct enemyImage));
         SDL_Surface *surface = IMG_Load(ENEMY_1_PATH);
 
-        if(!surface)
-        {
-            printf("Error: %s\n",SDL_GetError());
+        if (!surface) {
+            printf("Error: %s\n", SDL_GetError());
             return NULL;
         }
 
@@ -45,34 +42,34 @@ EnemyImage *initiateEnemy(SDL_Renderer *pRenderer)
         pEnemyImage->pTexture = SDL_CreateTextureFromSurface(pRenderer, surface);
         SDL_FreeSurface(surface);
 
-        if(!pEnemyImage->pTexture)
-        {
-            printf("Error: %s\n",SDL_GetError());
+        if (!pEnemyImage->pTexture) {
+            printf("Error: %s\n", SDL_GetError());
             return NULL;
         }
     }
     return pEnemyImage;
 }
 
-Enemy *createEnemy(EnemyImage *pEnemyImage, int window_width, int window_height){
+Enemy *createEnemy(EnemyImage *pEnemyImage, int window_width, int window_height) {
     Enemy *pEnemy = malloc(sizeof(struct enemy));
     pEnemy->pRenderer = pEnemyImage->pRenderer;
     pEnemy->pTexture = pEnemyImage->pTexture;
     pEnemy->window_width = window_width;
     pEnemy->window_height = window_height;
-    SDL_QueryTexture(pEnemyImage->pTexture,NULL,NULL,&(pEnemy->rect.w),&(pEnemy->rect.h));
+    SDL_QueryTexture(pEnemyImage->pTexture, NULL, NULL, &(pEnemy->rect.w), &(pEnemy->rect.h));
     getStartValues(pEnemy);
     pEnemy->active = true;
     return pEnemy;
 }
 
-Enemy *createEnemyOnClient(EnemyImage *pEnemyImage, int window_width, int window_height, Enemy_1_Data enemyData) {
+Enemy *createEnemyOnClient(EnemyImage *pEnemyImage, int window_width, int window_height,
+                           Enemy_1_Data enemyData) {
     Enemy *pEnemy = malloc(sizeof(struct enemy));
     pEnemy->pRenderer = pEnemyImage->pRenderer;
     pEnemy->pTexture = pEnemyImage->pTexture;
     pEnemy->window_width = window_width;
     pEnemy->window_height = window_height;
-    SDL_QueryTexture(pEnemyImage->pTexture,NULL,NULL,&(pEnemy->rect.w),&(pEnemy->rect.h));
+    SDL_QueryTexture(pEnemyImage->pTexture, NULL, NULL, &(pEnemy->rect.w), &(pEnemy->rect.h));
     getStartValuesFromServer(pEnemy, enemyData);
     return pEnemy;
 }
@@ -89,26 +86,23 @@ static void getStartValuesFromServer(Enemy *pEnemy, Enemy_1_Data enemyData) {
     pEnemy->active = enemyData.active;
 }
 
-static void getStartValues(Enemy *pEnemy){
-    int startSpawnOnTheLeft =  rand() % 2; //0 or 1
+static void getStartValues(Enemy *pEnemy) {
+    int startSpawnOnTheLeft = rand() % 2; // 0 or 1
     pEnemy->rectHitbox.x = pEnemy->rect.x + 10;
     pEnemy->rectHitbox.y = pEnemy->rect.y + 10;
     pEnemy->rectHitbox.w = pEnemy->rect.w - 20;
     pEnemy->rectHitbox.h = pEnemy->rect.h - 10;
     pEnemy->damage = 1;
     pEnemy->health = 2;
-    //float speed = rand() % (50 - 15 + 1) + 15;
+    // float speed = rand() % (50 - 15 + 1) + 15;
     float speed = INITIALSPEED;
-    if (startSpawnOnTheLeft)
-    {
+    if (startSpawnOnTheLeft) {
         pEnemy->x = pEnemy->window_width;
         pEnemy->y = rand() % (pEnemy->window_height - pEnemy->rect.h);
         pEnemy->vx = -speed; // rakt åt vänster
         pEnemy->vy = 0;      // ingen rörelse i y-led
 
-    }
-    else 
-    {
+    } else {
         pEnemy->x = 0; // also spawn at left
         pEnemy->y = rand() % (pEnemy->window_height - pEnemy->rect.h);
 
@@ -117,34 +111,31 @@ static void getStartValues(Enemy *pEnemy){
     }
 }
 
-SDL_Rect getRectEnemy(Enemy *pEnemy)
-{
-    if (pEnemy->active == true)
-    {
+SDL_Rect getRectEnemy(Enemy *pEnemy) {
+    if (pEnemy->active == true) {
         return pEnemy->rectHitbox;
     }
-    SDL_Rect empty = {0,0,0,0};
+    SDL_Rect empty = {0, 0, 0, 0};
     return empty;
 }
-//void updateEnemy(Enemy *pEnemy, Uint32 deltaTime){ //why deltaTime? if needed create it inside enemy_1.c beacause deltaTime we use in clientMain.c is messing up the enemy speed!
-void updateEnemy(Enemy *pEnemy){
-    if(pEnemy->active == true)
-    {
+// void updateEnemy(Enemy *pEnemy, Uint32 deltaTime){ //why deltaTime? if needed create it inside
+// enemy_1.c beacause deltaTime we use in clientMain.c is messing up the enemy speed!
+void updateEnemy(Enemy *pEnemy) {
+    if (pEnemy->active == true) {
         pEnemy->x += pEnemy->vx;
         pEnemy->y += pEnemy->vy;
         if (pEnemy->x > pEnemy->window_width || pEnemy->x + pEnemy->rect.w < 0 ||
-            pEnemy->y > pEnemy->window_height || pEnemy->y + pEnemy->rect.h < 0)
-        {
-            //getStartValues(pEnemy); //immediatly respawns enemy once it leaves windows
+            pEnemy->y > pEnemy->window_height || pEnemy->y + pEnemy->rect.h < 0) {
+            // getStartValues(pEnemy); //immediatly respawns enemy once it leaves windows
             pEnemy->active = false;
-            //printf("Enemy has fled \n");
+            // printf("Enemy has fled \n");
             return;
         }
         pEnemy->rect.x = pEnemy->x;
         pEnemy->rect.y = pEnemy->y;
         pEnemy->rectHitbox.x = pEnemy->rect.x + 10;
         pEnemy->rectHitbox.y = pEnemy->rect.y + 10;
-   }
+    }
 }
 
 void updateEnemyOnClients(Enemy *pEnemy, Enemy_1_Data enemyData) {
@@ -157,71 +148,56 @@ void updateEnemyOnClients(Enemy *pEnemy, Enemy_1_Data enemyData) {
     pEnemy->rectHitbox.y = pEnemy->rect.y + 10;
 }
 
-void drawEnemy(Enemy *pEnemy){
-    if (pEnemy->active == true)
-    {
-        SDL_RenderCopyEx(pEnemy->pRenderer, pEnemy->pTexture, NULL, &(pEnemy->rect), 0, NULL, SDL_FLIP_NONE); //made 0 to not rotate enemy.png
+void drawEnemy(Enemy *pEnemy) {
+    if (pEnemy->active == true) {
+        SDL_RenderCopyEx(pEnemy->pRenderer, pEnemy->pTexture, NULL, &(pEnemy->rect), 0, NULL,
+                         SDL_FLIP_NONE); // made 0 to not rotate enemy.png
     }
 }
 
-void destroyEnemy_1(Enemy *pEnemy){
-        free(pEnemy);
+void destroyEnemy_1(Enemy *pEnemy) {
+    free(pEnemy);
 }
 
-void destroyEnemy_1Image(EnemyImage *pEnemyImage){
+void destroyEnemy_1Image(EnemyImage *pEnemyImage) {
     SDL_DestroyTexture(pEnemyImage->pTexture);
 }
 
-void disableEnemy(Enemy *pEnemy)
-{
-    if(pEnemy->health <= 0)
-    {
-    pEnemy->active = false;
+void disableEnemy(Enemy *pEnemy) {
+    if (pEnemy->health <= 0) {
+        pEnemy->active = false;
     }
 }
 
-void damageEnemy(Enemy *pEnemy, int damage, int i)
-{
+void damageEnemy(Enemy *pEnemy, int damage, int i) {
     pEnemy->health -= damage;
-    if(pEnemy->health <= 0 && pEnemy->active == true)
-    {
-    pEnemy->active = false;
-    printf("Enemy nr %d dead\n", i);
+    if (pEnemy->health <= 0 && pEnemy->active == true) {
+        pEnemy->active = false;
+        printf("Enemy nr %d dead\n", i);
     }
     if (pEnemy->health < 0) pEnemy->health = 0;
 }
 
-bool isInWindow(Enemy *pEnemy)
-{
+bool isInWindow(Enemy *pEnemy) {
     if (pEnemy->x > pEnemy->window_width || pEnemy->x + pEnemy->rect.w < 0 ||
-        pEnemy->y > pEnemy->window_height || pEnemy->y + pEnemy->rect.h < 0)
-    {
-        disableEnemy(pEnemy);  
+        pEnemy->y > pEnemy->window_height || pEnemy->y + pEnemy->rect.h < 0) {
+        disableEnemy(pEnemy);
         return false;
-    }
-    else
-    {
+    } else {
         return true;
     }
 }
 
-bool isEnemyActive(Enemy *pEnemy)
-{
-    if (pEnemy->active == false)
-    {
+bool isEnemyActive(Enemy *pEnemy) {
+    if (pEnemy->active == false) {
         return false;
-    }
-    else
-    {
+    } else {
         return true;
     }
-
 }
-void printEnemyHealth(Enemy *pEnemy)
-{
-    if(pEnemy->active == true)
-    {
-        printf("Health: %d\n",pEnemy->health);
+void printEnemyHealth(Enemy *pEnemy) {
+    if (pEnemy->active == true) {
+        printf("Health: %d\n", pEnemy->health);
     }
 }
 
--- lib/src/stars.c
+++ lib/src/stars.c
@@ -4,31 +4,31 @@
 #include <stdlib.h>
 
 struct stars {
-  int nr_of_stars;
-  SDL_Point *pStarList;
+    int nr_of_stars;
+    SDL_Point *pStarList;
 };
 
 static void drawStar(SDL_Point *pPoint, SDL_Renderer *pRenderer);
 
 Stars *createStars(int nr_of_stars, int width, int height) {
-  srand(1);
-  Stars *s = malloc(sizeof(struct stars));
-  s->pStarList = malloc(nr_of_stars * sizeof(SDL_Point));
-  s->nr_of_stars = nr_of_stars;
-  for (int i = 0; i < nr_of_stars; i++) {
-    s->pStarList[i].x = rand() % width;
-    s->pStarList[i].y = rand() % height;
-  }
-  return s;
+    srand(1);
+    Stars *s = malloc(sizeof(struct stars));
+    s->pStarList = malloc(nr_of_stars * sizeof(SDL_Point));
+    s->nr_of_stars = nr_of_stars;
+    for (int i = 0; i < nr_of_stars; i++) {
+        s->pStarList[i].x = rand() % width;
+        s->pStarList[i].y = rand() % height;
+    }
+    return s;
 }
 
 void drawStars(Stars *pstars, SDL_Renderer *pRenderer) {
-  for (int i = 0; i < pstars->nr_of_stars; i++) {
-    drawStar(&(pstars->pStarList[i]), pRenderer);
-  }
+    for (int i = 0; i < pstars->nr_of_stars; i++) {
+        drawStar(&(pstars->pStarList[i]), pRenderer);
+    }
 }
 
-//white dots (circles)
+// white dots (circles)
 /*static void drawStar(SDL_Point *pPoint, SDL_Renderer *pRenderer) {
   SDL_RenderDrawPoint(pRenderer, pPoint->x, pPoint->y - 1);
   SDL_RenderDrawPoint(pRenderer, pPoint->x + 1, pPoint->y - 1);
@@ -44,23 +44,22 @@ void drawStars(Stars *pstars, SDL_Renderer *pRenderer) {
   SDL_RenderDrawPoint(pRenderer, pPoint->x + 1, pPoint->y + 2);
 }*/
 
-//Lines radiating outward from center (5-pointed)
+// Lines radiating outward from center (5-pointed)
 static void drawStar(SDL_Point *p, SDL_Renderer *pRenderer) {
-  int x1 = p->x;
-  int y1 = p->y;
+    int x1 = p->x;
+    int y1 = p->y;
 
-  
-  SDL_RenderDrawLine(pRenderer, x1, y1, x1, y1 - 3);   
-  SDL_RenderDrawLine(pRenderer, x1, y1, x1 + 3, y1);   
-  SDL_RenderDrawLine(pRenderer, x1, y1, x1, y1 + 3);   
-  SDL_RenderDrawLine(pRenderer, x1, y1, x1 - 3, y1);   
-  SDL_RenderDrawLine(pRenderer, x1, y1, x1 + 2, y1 - 2); 
-  SDL_RenderDrawLine(pRenderer, x1, y1, x1 - 2, y1 - 2); 
-  SDL_RenderDrawLine(pRenderer, x1, y1, x1 + 2, y1 + 2); 
-  SDL_RenderDrawLine(pRenderer, x1, y1, x1 - 2, y1 + 2); 
+    SDL_RenderDrawLine(pRenderer, x1, y1, x1, y1 - 3);
+    SDL_RenderDrawLine(pRenderer, x1, y1, x1 + 3, y1);
+    SDL_RenderDrawLine(pRenderer, x1, y1, x1, y1 + 3);
+    SDL_RenderDrawLine(pRenderer, x1, y1, x1 - 3, y1);
+    SDL_RenderDrawLine(pRenderer, x1, y1, x1 + 2, y1 - 2);
+    SDL_RenderDrawLine(pRenderer, x1, y1, x1 - 2, y1 - 2);
+    SDL_RenderDrawLine(pRenderer, x1, y1, x1 + 2, y1 + 2);
+    SDL_RenderDrawLine(pRenderer, x1, y1, x1 - 2, y1 + 2);
 }
 
 void destroyStars(Stars *pStars) {
-  free(pStars->pStarList);
-  free(pStars);
+    free(pStars->pStarList);
+    free(pStars);
 }
