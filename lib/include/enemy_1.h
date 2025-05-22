90c901d617c808c15a9f09f3a6242b486d0b8211
Erik-Falk <falk.erik@live.se>
2025-05-22 11:21:35 +0200

    formating

--- lib/include/enemy_1.h
+++ lib/include/enemy_1.h
@@ -5,14 +5,13 @@
 
 #include "ship_data.h"
 
-
 typedef struct enemyImage EnemyImage;
 typedef struct enemy Enemy;
 
 EnemyImage *initiateEnemy(SDL_Renderer *pRenderer);
 
 Enemy *createEnemy(EnemyImage *pEnemyImage, int window_width, int window_height);
-//void updateEnemy(Enemy *pEnemy, Uint32 deltaTime);
+// void updateEnemy(Enemy *pEnemy, Uint32 deltaTime);
 void updateEnemy(Enemy *pEnemy);
 void drawEnemy(Enemy *pEnemy);
 void destroyEnemy_1(Enemy *pEnemy);
@@ -24,10 +23,10 @@ bool isInWindow(Enemy *pEnemy);
 bool isEnemyActive(Enemy *pEnemy);
 void printEnemyHealth(Enemy *pEnemy);
 void updateEnemyOnClients(Enemy *pEnemy, Enemy_1_Data enemyData);
-Enemy *createEnemyOnClient(EnemyImage *pEnemyImage, int window_width, int window_height, Enemy_1_Data enemyData);
+Enemy *createEnemyOnClient(EnemyImage *pEnemyImage, int window_width, int window_height,
+                           Enemy_1_Data enemyData);
 static void getStartValuesFromServer(Enemy *pEnemy, Enemy_1_Data enemyData);
 
-void getEnemy_1_DataPackage(Enemy* pEnemy, Enemy_1_Data* pEnemyData);
-
+void getEnemy_1_DataPackage(Enemy *pEnemy, Enemy_1_Data *pEnemyData);
 
 #endif
